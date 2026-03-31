2136c9e6e3939e24817b371b02ec2c53f97858d6
DevBrigid <mukamibrigid20@gmail.com>
2026-03-31 11:19:19 +0300

    complete scope assessment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Write your solution in this file!
+const burgers = ["Hamburger","Cheeseburger"];
+const featuredDrink = ["Strawaberry Milkshake"];
+
+function addBurger () {
+    let newBurger = "Flatburger";
+    burgers.push(newBurger);
+
+    if (true) {
+        anotherNewBurger = "Maple Bacon Burger";
+        burgers.push(anotherNewBurger);
+    }
+}
+
+function changeFeaturedDrink (){
+    featuredDrink.pop();
+    featuredDrink.push("Javashake");
+}
+
+addBurger();
