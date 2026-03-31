99fb77ff2a04a06566465a4ca65460328fb0c317
DevBrigid <mukamibrigid20@gmail.com>
2026-03-31 11:40:45 +0300

    fix inconsistencies

--- index.js
+++ index.js
@@ -1,20 +1,16 @@
 // Write your solution in this file!
 const burgers = ["Hamburger","Cheeseburger"];
-const featuredDrink = ["Strawaberry Milkshake"];
+let featuredDrink = "Strawberry Milkshake";
 
 function addBurger () {
-    let newBurger = "Flatburger";
+    const newBurger = "Flatburger";
     burgers.push(newBurger);
-
-    if (true) {
-        anotherNewBurger = "Maple Bacon Burger";
+    if(true){
+        const anotherNewBurger = "Maple Bacon Burger";
         burgers.push(anotherNewBurger);
     }
 }
 
 function changeFeaturedDrink (){
-    featuredDrink.pop();
-    featuredDrink.push("Javashake");
+    featuredDrink = "The JavaShake";
 }
-
-addBurger();
